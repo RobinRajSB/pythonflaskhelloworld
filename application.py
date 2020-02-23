835d8ea46e46bce107bf85698d83de57fdc5f08c
Danny Ky <Dannyky@users.noreply.github.com>
2020-02-23 19:45:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,redirect, url_for, request
-#import gspread
-#from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 app = Flask(__name__)
 
 @app.route("/")
