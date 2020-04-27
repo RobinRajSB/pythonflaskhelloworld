ed3dbd2fe1af09b0c2a49211841b11e300045066
Robin Raj SB <robinraj.sb@gmail.com>
2020-04-27 21:41:04 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,redirect, url_for, request
 import tensorflow as tf
-import gspread
+
 from oauth2client.service_account import ServiceAccountCredentials
 app = Flask(__name__)
 
